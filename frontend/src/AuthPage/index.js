54f483540893291594d64f0896ad32e8c50ca678
Kyle <kylevhuang@gmail.com>
2022-01-05 13:39:58 -0800

    authpage

--- frontend/src/AuthPage/index.js
+++ frontend/src/AuthPage/index.js
@@ -1,15 +1,24 @@
 import "./authpage.css";
-
+import {Link} from "react-router-dom"
 function AuthPage() {
   return (
-  <div>
-      <form>
-          <label type="text" required>Email</label>
-          <input></input>
-          <label type="text" required>Password</label>
-          <input></input>
-      </form>
-  </div>
+    <div className="wrapper authwrapper">
+      <div className="auth">
+        <div className="namelogo">
+          <img src="./zinglogo.png" alt="logo"></img>
+          <h2>Zing</h2>
+        </div>
+        <p> Sign in to your workspace.</p>
+        <Link to="/signup">Don't have an account?</Link>
+        <form onSubmit={(e) => {
+            e.preventDefault()
+        }}>
+          <input type="text" placeholder={"Email"} required></input>
+          <input type="password" placeholder={"Password"} required></input>
+          <button>Continue</button>
+        </form>
+      </div>
+    </div>
   );
 }
 
