9d59deed31851b16035740e2f04094f0131fd368
Kyle <kylevhuang@gmail.com>
2022-01-05 09:10:34 -0800

    fixed database relations

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,40 +1,101 @@
-
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
-
 db = SQLAlchemy()
 
-## Through tables go here (groups,members) 
-## for many to many relationships
 
-class Users(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = "users"
-    id = db.Column(db.Integer,primary_key=True)
-    name = db.Column(db.String(50),nullable=False)
-    username = db.Column(db.String(50),nullable=False)
-    email = db.Column(db.String(50),nullable=False)
-    hashedPassword = db.Column(db.String(300),nullable=False)
-    profilePicture = db.Column(db.Text)
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    hashedPassword = db.Column(db.String(300), nullable=False)
+    profilePicture = db.Column(db.String)
     status = db.Column(db.String(200))
 
-    messages = db.relationship("Messages",back_populates="user")
+    messages = db.relationship("Message", back_populates="owners", cascade='all, delete')
+    organization = db.relationship("Organization", back_populates="owner", cascade='all, delete')
+    direct_messages = db.relationship('DirectMessage', back_populates='owner', cascade='all, delete')
+    members = db.relationship('Organization', secondary='members', back_populates='users', cascade='all, delete')
+    groups = db.relationship('Channel', secondary='groups', back_populates='users', cascade='all, delete')
 
-##todo
-class Organizations(db.Model):
+    @property
+    def password(self):
+        return self.hashedPassword
+
+    @password.setter
+    def password(self, password):
+        self.hashedPassword = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+
+class Organization(db.Model):
     __tablename__ = "organizations"
 
-##todo
-class Channels(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    owner = db.relationship("User", back_populates="organization", cascade='all, delete')
+    channels = db.relationship("Channel", back_populates="organizations", cascade='all, delete')
+    direct_messages = db.relationship('DirectMessage', back_populates='organizations', cascade='all, delete')
+    members = db.relationship('User', secondary='members', back_populates='organization', cascade='all, delete')
+
+
+class Channel(db.Model):
     __tablename__ = "channels"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    org_id = db.Column(db.Integer, db.ForeignKey('organizations.id'), nullable=False)
 
-##todo
-class DirectMessages(db.Model):
-    __tablename__ = "directmessages"
+    organizations = db.relationship("Organization", back_populates="channels")
+    channel_messages = db.relationship('Message', back_populates='user_channels')
+    groups = db.relationship('User',secondary='groups',back_populates="channels")
 
-class Messages(db.Model):
-    __tablename__ = "messages"
+class DmChannel(db.Model):
+    __tablename__ = "dmchannels"
     id = db.Column(db.Integer,primary_key=True)
-    content = db.Column(db.Text,nullable=False)
-    owner_id = db.Column(db.Integer,ForeignKey("users.id"),nullable=False)
-    channel_id = db.Column(db.Integer,ForeignKey("channels.id"),nullable=False)
+    firstUser_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    secondUser_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
-    user = db.relationship("Users",back_populates="messages")
+    direct_messages = db.relationship('DirectMessage',back_populates="dm_channels")
+
+class DirectMessage(db.Model):
+    __tablename__ = "directmessages"
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(500), nullable=False)
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    dm_channel_id = db.Column(db.Integer, db.ForeignKey('dmchannels.id'), nullable=False)
+
+    owner = db.relationship("User", back_populates="direct_messages")
+    dm_channels = db.relationship("DmChannel",back_populates="direct_messages")
+
+
+class Message(db.Model):
+    __tablename__ = "messages"
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    owner_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    channel_id = db.Column(db.Integer, db.ForeignKey("channels.id"), nullable=False)
+
+    owners = db.relationship("User", back_populates="messages")
+    user_channels = db.relationship("Channel", back_populates="channel_messages")
+
+
+class Member(db.Model):
+    __tablename__ = 'members'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    org_id = db.Column(db.Integer, db.ForeignKey('organizations.id'), nullable=False)
+
+
+class Group(db.Model):
+    __tablename__ = "groups"
+
+    id = db.Column(db.Integer,primary_key=True)
+    channel_id = db.Column(db.Integer,db.ForeignKey('channels.id'),nullable=False)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'),nullable=False)
