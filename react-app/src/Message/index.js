37229c4f9e55d0b475f31150156c4e3dbdb38585
felmallakh <fady_elmallakh@hotmail.com>
2022-01-11 11:01:38 -0800

    structure

---
+++ react-app/src/Message/index.js
@@ -0,0 +1,65 @@
+import "./message.css";
+import { useState, useEffect, useRef } from "react";
+import io from "socket.io-client";
+
+// must use http here
+//"https://<herokuname>.herokuapp.com" for heroku
+let endPoint = "https://zing-app.herokuapp.com";
+let socket;
+function Message() {
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState("");
+  const dummyDiv = useRef(null)
+  useEffect(() => {
+    socket = io(`${endPoint}`)
+    getMessages();
+    return (() =>{
+      socket.disconnect()
+    })
+  }, [messages.length]);
+
+  const getMessages = () => {
+    socket.once("message", (msg) => {
+      setMessages([...messages, msg]);
+      dummyDiv.current.scrollIntoView(false)
+    });
+  };
+  const onChange = (e) => {
+    setMessage(e.target.value);
+  };
+
+  const onClick = () => {
+    if (message !== "") {
+      socket.emit("message", message);
+      setMessage("");
+    } else {
+      alert("Please add message");
+    }
+  };
+  return (
+
+    <div className="messageArea">
+      <div className="title">
+        <h2>Title</h2>
+      </div>
+      <div className="messages">
+        <div>
+          {messages.map((msg) => {
+            return <p>{msg}</p>;
+          })}
+          <p ref={dummyDiv}></p>
+        </div>
+      </div>
+      <div className="inputMessages">
+        <form onSubmit={(e) => {
+          e.preventDefault();
+          onClick();
+        }}>
+          <input value={message} onChange={(e) => onChange(e)} placeholder={"Message"}></input>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Message;
