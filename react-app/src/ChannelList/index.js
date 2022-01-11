2fb2e80292b8e7d01bf2c1f627c30b38d8acdf06
Enoch Tan <enochtan@umich.edu>
2022-01-11 18:38:00 -0500

    edit channel form done

--- react-app/src/ChannelList/index.js
+++ react-app/src/ChannelList/index.js
@@ -1,13 +1,16 @@
 import "./channelList.css";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { readChannels } from "../store/channels";
+import { useNavigate } from 'react-router-dom'
 
 function ChannelList() {
     const [showEdit, setShowEdit] = useState(false)
     const [showForm, setShowForm] = useState(false)
     const [showh3, setShowh3] = useState(true)
+    const [channelName, setChannelName] = useState('')
+    const input = useRef(null)
 
     const { id } = useParams()
     const dispatch = useDispatch();
@@ -21,12 +24,32 @@ function ChannelList() {
         loadChannels();
     }, [dispatch]);
 
+    const editToggle = () => {
+        if (showForm === true) return;
+        if (showEdit === false) return setShowEdit(true);
+        if (showEdit === true) return setShowEdit(false);
+      }
+
     return (
         <div className="channelContainer">
             {channels ? channels.map((channel) => {
                 return (
-                    <div className="singleChannel" key={channel.id}>
-                        <h3># {channel.name}</h3>
+                    <div onClick={editToggle} className="singleChannel" key={channel.id}>
+                        <h3># {showh3 && channel.name}</h3>
+                        { showForm && (
+                            <div>
+                                <form className="editchannelform" onSubmit={ handleSubmit }>
+                                    <input
+                                        type='text'
+                                        ref={input}
+                                        placeholder={"New Name"}
+                                        required
+                                        value={channelName}
+                                        onChange={e => setChannelName(e.target.value)}
+                                    />
+                                </form>
+                            </div>
+                        )}
                     </div>
                 );
             }) : null}
