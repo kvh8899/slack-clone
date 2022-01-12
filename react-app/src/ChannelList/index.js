28d09c5b3713cf738a62a679635ff88906f05576
felmallakh <fady_elmallakh@hotmail.com>
2022-01-11 17:12:24 -0800

    channel-delete

--- react-app/src/ChannelList/index.js
+++ react-app/src/ChannelList/index.js
@@ -2,32 +2,39 @@ import "./channelList.css";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
-import { readChannels } from "../store/channels";
+import { readChannels, removeChannel } from "../store/channels";
 
 function ChannelList() {
-    const { id } = useParams()
-    const dispatch = useDispatch();
-    const channels = useSelector((state) => state.channelReducer);
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const channels = useSelector((state) => state.channelReducer);
 
-    async function loadChannels() {
-        await dispatch(readChannels(id))
-    }
+  async function loadChannels() {
+    await dispatch(readChannels(id));
+  }
 
-    useEffect(() => {
-        loadChannels();
-    }, [dispatch]);
+  useEffect(() => {
+    loadChannels();
+  }, [dispatch]);
 
-    return (
-        <div className="channelContainer">
-            {channels ? channels.map((channel) => {
-                return (
-                    <div className="singleChannel" key={channel.id}>
-                        <h3># {channel.name}</h3>
-                    </div>
-                );
-            }) : null}
-        </div>
-    );
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    await dispatch(removeChannel(e.target.id));
+  }
+
+  return (
+    <div className="channelContainer">
+      {channels ? channels.map((channel) => {
+            return (
+              <div className="singleChannel" key={channel.id}>
+                <h3># {channel.name}</h3>
+                <i class="fas fa-trash-alt" onClick={handleSubmit} id={channel.id}></i>
+              </div>
+            );
+          })
+        : null}
+    </div>
+  );
 }
 
 export default ChannelList;
