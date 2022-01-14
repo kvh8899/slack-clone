71715131a3d9f1faef680d61f57f528c5bafd7ea
Kyle <kylevhuang@gmail.com>
2022-01-14 11:27:47 -0800

    mobile scaling for chat

--- react-app/src/MessageBar/index.js
+++ react-app/src/MessageBar/index.js
@@ -13,6 +13,7 @@ function MessageBar({ setSelectedChannel, setSelectedChannelId }) {
   const dCaret = useRef(null);
   const size = useRef(null);
   const size1 = useRef(null);
+  const msgBar = useRef(null);
   const dispatch = useDispatch();
   const { id } = useParams();
   const org = useSelector((state) => state.orgmainchatReducer);
@@ -21,9 +22,27 @@ function MessageBar({ setSelectedChannel, setSelectedChannelId }) {
     dispatch(getOrg(id));
   }, []);
 
+  useEffect(() => {
+    function onResize(){
+      let r = document.querySelector(".r")
+      if(r.clientWidth > document.body.clientWidth * 0.5){
+        size.current.classList.add("hide")
+        size1.current.classList.add("hide")
+        msgBar.current.classList.add("hide")
+        r.classList.add("abs")
+        
+      }else if(r.clientWidth < document.body.clientWidth * 0.5){
+        size.current.classList.remove("hide")
+        size1.current.classList.remove("hide")
+        msgBar.current.classList.remove("hide")
+        r.classList.remove("abs")
+      }
+    }
+    window.onresize = onResize;
+  },[])
   return (
     <>
-      <div className="messageBar">
+      <div className="messageBar" ref={msgBar}>
         <div className="title">
           <h2 className="unselect">{org.name}</h2>
           <div
@@ -107,6 +126,7 @@ function MessageBar({ setSelectedChannel, setSelectedChannelId }) {
               size.current.style.width = (e.clientX + 2 )+ "px";
               size1.current.style.width = (e.clientX + 2) + "px";
             }
+
           });
         }}
       ></div>
